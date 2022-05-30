46a7563175a187dc3d3286c9c468f460c4b52414
Michael <123berlioz@gmail.com>
2022-05-30 13:16:36 -0400

    initial repo commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homescreen from './screens/Homescreen';
+import Productscreen from './screens/Productscreen';
 function App() {
   return (
-    <>
-      <h1>Welcome To ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<Homescreen />} />
+            <Route path="/product/:id" element={<Productscreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/Homescreen.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Homescreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((p) => (
+            <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={p} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default Homescreen;
